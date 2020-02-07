cfb346d1345a86853f9a569263a690545a248ee0
YourMeaning <59411682+YourMeaning@users.noreply.github.com>
2020-02-07 14:46:07 +0900

    0207

---
+++ 02_college/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.login').on('submit', function(){
+        var id = $('#id').val();
+        var pw = $('#pw').val();
+//        if(id != '' && pw != ''){
+            location.href="sub/sub.html";
+//        }
+        
+    })
+    
+    
+    
+    
+    
+}
