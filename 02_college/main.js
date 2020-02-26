25cf6e9ed8aca34569d8f1256b8298b6a969c45a
YourMeaning <59411682+YourMeaning@users.noreply.github.com>
2020-02-26 14:22:28 +0900

    02.26
    02.26

--- 02_college/main.js
+++ 02_college/main.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-    $('.login').on('submit', function(){
-        var id = $('#id').val();
-        var pw = $('#pw').val();
-        if(id != '' && pw != ''){
-            window.location.href = 'sub/sub';
-        }
-        
-    })
+    
     
     
     
